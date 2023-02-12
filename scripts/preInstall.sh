63aade9ff17ffe7e51ca45b5333b78adbbd414cd
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-12 11:32:53 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,6 +1,11 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
+ip="172.17.0.1"
 
+    sed "s,__IP__,$ip," template/dfget.template.yaml > config/dfget.yaml
+    sed "s,__IP__,$ip," template/seed-peer.template.yaml > config/seed-peer.yaml
+    sed "s,__IP__,$ip," template/scheduler.template.yaml > config/scheduler.yaml
+    sed "s,__IP__,$ip," template/manager.template.yaml > config/manager.yaml
 
 # mkdir -p ./data
 # chmod a+w data

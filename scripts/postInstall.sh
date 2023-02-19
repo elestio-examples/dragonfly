e1cfa1a4e61d78d48b7f81a4602d9bfd906a12ba
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-19 14:08:54 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -29,11 +29,21 @@ access_token=$(echo $login | jq -r '.token' )
   --compressed
 
 
-curl http://${target}/api/v1/users/1/reset_password \
+
+curl https://${DOMAIN}/api/v1/users/1/reset_password \
+-H 'authority: '${DOMAIN}'' \
   -H 'accept: application/json' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
   -H 'content-type: application/json;charset=UTF-8' \
   -H 'cookie: jwt='${access_token}'' \
+  -H 'origin: https://'${DOMAIN}'' \
+  -H 'referer: https://'${DOMAIN}'/profile/1' \
+  -H 'sec-ch-ua: "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
   --data-raw '{"old_password":"dragonfly","new_password":"'${ADMIN_PASSWORD}'","confirm_password":"'${ADMIN_PASSWORD}'"}' \
   --compressed

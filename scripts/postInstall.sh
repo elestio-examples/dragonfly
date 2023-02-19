27635b493961bc9db3531f197d87744dc308e703
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-19 12:49:25 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -7,6 +7,8 @@ sleep 60s;
 
 target=$(docker-compose port manager 8080)
 
+curl http://${target}/installation
+
 login=$(curl http://${target}/api/v1/users/signin \
   -H 'accept: application/json' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
@@ -15,11 +17,22 @@ login=$(curl http://${target}/api/v1/users/signin \
   --data-raw '{"name":"root","password":"dragonfly"}' \
   --compressed)
 
-
 access_token=$(echo $login | jq -r '.token' )
+  
+  curl http://${target}/api/v1/configs \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json;charset=UTF-8' \
+  -H 'cookie: jwt='${access_token}'' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  --data-raw '{"name":"is_boot","value":"1","user_id":1}' \
+  --compressed
 
 
 curl http://${target}/api/v1/users/1/reset_password \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json;charset=UTF-8' \
   -H 'cookie: jwt='${access_token}'' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
   --data-raw '{"old_password":"dragonfly","new_password":"'${ADMIN_PASSWORD}'","confirm_password":"'${ADMIN_PASSWORD}'"}' \
